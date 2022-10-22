c606cc4c4a92f0507fb748a8c69154a06b88ec98
gentry <gma23@txstate.edu>
2022-10-22 18:40:02 -0500

    learn one channel at a time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from model_wrappers import NNCLR_C
+from model_wrappers import NNCLR_R
 from sklearn.model_selection import train_test_split
 import torch
 
@@ -16,6 +16,12 @@ FLOW = 9
 THO = 10
 ABD = 11
 
+chan_dic = {
+    'ECG' : [8],
+    'Thermistor': [9],
+    'Respiratory Belt' : [10, 11]
+}
+
 
 if __name__ == '__main__':
     print('Reading arrays...')
@@ -29,18 +35,27 @@ if __name__ == '__main__':
     print('X val shape: ', x_val.shape)
     print('X test shape: ', x_test.shape)
 
-    x_all = x_all[:,:,[ECG, FLOW, THO, ABD]]
+    #x_all = x_all[:,:,[ECG, FLOW, THO, ABD]]
+    #Swapping to channels first
     x_all = np.moveaxis(x_all, 2, 1)
     print('X all shape: ', x_all.shape) #expected 90210
 
     
 
     x_all_train, x_all_val = train_test_split(x_all, test_size=0.2, random_state=1899, shuffle=True)
-    feature_learner = NNCLR_C(x_all_train, np.ones((x_all_train.shape[0])))
-    feature_learner.fit(
-        x_all_train, np.ones(x_all_train.shape[0]),x_all_val, np.ones(x_all_val.shape[0])
-    )
-
-    torch.save(feature_learner, 'feature_learner_weights.pth')
+    for key in chan_dic.keys():
+        isolated_channel_train = x_all_train[:,chan_dic[key],:]
+        isolated_channel_val = x_all_val[:,chan_dic[key],:]
+        print('Isolated channel train shape: ', isolated_channel_train.shape)
+        print('Isolated channel validation shape: ', isolated_channel_val.shape)
+        
+        feature_learner = NNCLR_R(isolated_channel_train, np.ones((isolated_channel_train.shape[0])))
+        feature_learner.fit(
+            isolated_channel_train, np.ones(isolated_channel_train.shape[0]),
+            isolated_channel_val, np.ones(isolated_channel_val.shape[0])
+        )
+        torch.save(feature_learner, f'{key}_feature_learner_weights.pth')
+        np.save(f'{key}_features_sub_50to100.npy')
+        
 
 
